78a6824dc8506ee5b96f78a94b4132e127a8fd9a
Francesco Berardinucci <francesco.berardinucci@hotmail.it>
2021-01-22 07:48:51 +0100

    sections aggiust

--- jsimIO/sections.py
+++ jsimIO/sections.py
@@ -2,29 +2,30 @@ from .xmlnode_superclass import *
 
 
 class Section(XmlNode):
-    def __init__(self, model):
+    def __init__(self, model, name, className):
         super().__init__("section")
         self.model = model
+        self.name = name
+        self.className = className
 
 
 #  STATION
 class Queue(Section):  # is a section
     def __init__(self, model, name, scheduling_strategy=None):
-        super().__init__(model)
-        print(model.__dict__)
-        self.className = "Queue"
+        super().__init__(model, name, "Queue")
+        self.scheduling_strategy = scheduling_strategy
 
 
 class Server(Section):
     def __init__(self, model, name, service_strategy=None):
-        super().__init__(model)
-        self.className = "Server"
+        super().__init__(model, name, "Server")
+        self.service_strategy = service_strategy
 
 
 class Router(Section):
     def __init__(self, model, name, routing_strategy=None):
-        super().__init__(model)
-        self.className = "Router"
+        super().__init__(model, name, "Router")
+        self.routing_strategy = routing_strategy
 
 
 #  SOURCE
