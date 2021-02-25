9524880fa1ddf969b2d731c106b315806d10aa80
Francesco Berardinucci <francesco.berardinucci@hotmail.it>
2021-02-25 17:05:46 +0100

    clean

--- jsimIO/__init__.py
+++
@@ -1,8 +0,0 @@
-from .connections import _Connection
-from .distributions import *
-from .model import *
-from .nodes import *
-# from .sections import *
-from .strategies import *
-from .userclasses import *
-# from .utilities import *
